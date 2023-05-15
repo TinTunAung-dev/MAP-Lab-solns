8a1aa03986407207ebb0aefa368b9db18044d453
Tin Tun Aung <tintunaung.tta.1993@gmail.com>
2023-05-15 16:22:54 -0500

    Lab7

--- Lab7/Exercise1.js
+++ Lab7/Exercise1.js
@@ -1,1 +1,14 @@
-let i = 0;
+const isPrime = num => new Promise((resolve, reject) => {
+    setTimeout(function() {
+        for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+            if(num % i === 0) reject({ prime: false }); 
+        resolve({ prime: num > 1 });
+    }, 500);
+});
+
+
+console.log('start');
+isPrime(7)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+console.log('end');
